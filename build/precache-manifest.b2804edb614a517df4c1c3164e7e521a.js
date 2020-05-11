daa508010408ce1d7f53c59009d0aa1326a906dd
Nadir Ali <alinadir0509@gmail.com>
2020-05-11 21:08:21 +0530

    adding build

---
+++ build/precache-manifest.b2804edb614a517df4c1c3164e7e521a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c8e0c2223dec3d308f86e66a0a6e1c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd8e750809b40df49c00",
+    "url": "/static/js/2.1aa9232f.chunk.js"
+  },
+  {
+    "revision": "1260a01290ac0c0454e9a7627323041c",
+    "url": "/static/js/2.1aa9232f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2278a6b7b4401146312",
+    "url": "/static/js/main.5beb300a.chunk.js"
+  },
+  {
+    "revision": "534ade4f578f06e14a8b",
+    "url": "/static/js/runtime-main.0b615a34.js"
+  }
+]);
